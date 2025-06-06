534c6aedc4fecea622762cc89ba7900decc6369f
ashwin2-mv <ashwinmarangad@gmail.com>
2025-06-06 13:41:18 +0530

    fixed formating

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { ToastContainer } from "react-toastify";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-            <ToastContainer />
-
-  </StrictMode>,
+    <ToastContainer />
+  </StrictMode>
 );
